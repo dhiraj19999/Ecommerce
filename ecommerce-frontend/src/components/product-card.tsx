485ef7d9411ba67fc29675c0721e983d105189f7
dhiraj19999 <yogeshsamse682@gmail.com>
2024-01-14 22:40:24 +0530

    secondday

---
+++ ecommerce-frontend/src/components/product-card.tsx
@@ -0,0 +1,34 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProductProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+const server = "fhbfbd";
+function Productcard({
+  productId,
+  photo,
+  name,
+  price,
+  stock,
+  handler,
+}: ProductProps) {
+  return (
+    <div className="product-card">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>{price}</span>
+      <div>
+        <button onClick={() => handler()}>
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Productcard;
--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import Productcard from "../components/product-card";
 function Home() {
-  return <div>Home</div>;
+  const addToCartHandler = () => {};
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link className="findmore" to={"/search"}>
+          More
+        </Link>
+      </h1>
+
+      <main className="main">
+        <Productcard
+          productId="jsjs"
+          name="mabook"
+          price={123}
+          stock={6}
+          handler={addToCartHandler}
+          photo="https://images.samsung.com/is/image/samsung/assets/in/computers/galaxy-book3/14951_GB3-PDP-banner_1440x450.jpg"
+        />
+      </main>
+    </div>
+  );
 }
 
 export default Home;
